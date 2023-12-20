2e90ea6cc5df66cef3fd29224b3b827b95b47b27
Shahrizan <shahrizan.learner@gmail.com>
2023-12-20 10:39:19 +0700

    add view bank

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,7 +2,10 @@ module.exports = {
   renderViewDashboard: (req, res) => {
     res.render('admin/dashboard/view_dashboard');
   },
-  renderviewCategory: (req, res) => {
+  renderViewCategory: (req, res) => {
     res.render('admin/category/view_category');
+  },
+  renderViewBank: (req, res) => {
+    res.render('admin/bank/view_bank');
   },
 };
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const adminController = require('../controllers/adminController');
 
 router.get('/dashboard', adminController.renderViewDashboard);
-router.get('/category', adminController.renderviewCategory);
+router.get('/category', adminController.renderViewCategory);
+router.get('/bank', adminController.renderViewBank);
 
 module.exports = router;
